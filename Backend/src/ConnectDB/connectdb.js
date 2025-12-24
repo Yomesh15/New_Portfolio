87cf599db8681691838fa376cb75dc6ffef655a2
Yomesh15 <yomeshnagar2006@gmail.com>
2025-12-24 21:14:45 +0530

    Yomesh's Portfolio

--- Backend/src/ConnectDB/connectdb.js
+++ Backend/src/ConnectDB/connectdb.js
@@ -1,18 +1,14 @@
 import mongoose from "mongoose";
 
-let isConnected = false;
-
 const connectDB = async () => {
-  if (isConnected) return;
   try {
     await mongoose.connect(process.env.MONGO_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    isConnected = true;
     console.log("MongoDB connected");
   } catch (err) {
-    console.error("MongoDB connection failed:", err);
+    console.error("Database connection failed", err);
     throw new Error("Database connection failed");
   }
 };
