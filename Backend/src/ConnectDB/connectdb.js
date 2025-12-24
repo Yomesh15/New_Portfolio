0cf78632e5f42fb6432fc728ff13e65801597b57
Yomesh15 <yomeshnagar2006@gmail.com>
2025-12-24 20:58:14 +0530

    change

--- Backend/src/ConnectDB/connectdb.js
+++ Backend/src/ConnectDB/connectdb.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
-let isConnected = false;
+let isConnected = false; 
 
 const connectDB = async () => {
   if (isConnected) return;
 
   try {
-    await mongoose.connect(process.env.MONGO_URL, {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
@@ -14,9 +14,8 @@ const connectDB = async () => {
     console.log("MongoDB connected");
   } catch (err) {
     console.error("MongoDB connection failed:", err);
-    throw new Error("Database connection failed");
+    throw new Error("Database connection failed"); 
   }
 };
-
 
 export default connectDB;
